e2d6681f457405192327d283da7b3109df7fbd0b
Chuhan Yu <cy366@cornell.edu>
2021-11-18 10:52:33 -0500

    add deploy

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Main from "./main";
 import Header from "./components/Header";
 import React from "react";
 import QuizButton from "./components/QuizButton";
+import { HashRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <QuizButton />
-    </div>
+    <HashRouter basename="/">
+      <div className="App">
+        <Header />
+        <Main />
+        <QuizButton />
+      </div>
+    </HashRouter>
   );
 }
 
