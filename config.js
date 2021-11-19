a636178a079b43a8c298f5c6ca48897f20675282
Zhan Wu <alex.zhan.wu@icloud.com>
2021-11-18 20:17:03 -0500

    Updates

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  apiKey: "AIzaSyBZ7l-yoDioqNoZCwzoUDYxXsUNVjGRXOk",
-  authDomain: "tns-beta-18.firebaseapp.com",
-  databaseURL: "https://tns-beta-18-default-rtdb.firebaseio.com",
-};
-
-export default config;
--- src/components/Quiz.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Widget } from "react-typeform-embed";
-
-const QuizWidget = () => {
-  return <Widget id="a5pkC39o" height={700} />;
-};
-export default QuizWidget;
