a7fa3557d4284c18295f291a7821158c36fc145f
Zhan Wu <alex.zhan.wu@icloud.com>
2021-11-24 22:30:14 -0500

    update database url

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDgDyPIPUacjs4bbM9NqvjQAFNuBZ8X0R4",
-  authDomain: "tns-project-62d54.firebaseapp.com",
-  databaseURL: "https://tns-project-62d54-default-rtdb.firebaseio.com",
-  projectId: "tns-project-62d54",
-  storageBucket: "tns-project-62d54.appspot.com",
-  messagingSenderId: "276200281067",
-  appId: "1:276200281067:web:8f2b4e8598e2d13bf6ffae",
-  measurementId: "G-7V3W7E5X7B"
+  apiKey: "AIzaSyCAW42Hkw8bUreEaZ8c4TmRSubsvOYjqC8",
+  authDomain: "tns-project-new.firebaseapp.com",
+  databaseURL: "https://tns-project-new-default-rtdb.firebaseio.com",
+  projectId: "tns-project-new",
+  storageBucket: "tns-project-new.appspot.com",
+  messagingSenderId: "1048019830484",
+  appId: "1:1048019830484:web:6a9e66e81f85b07b38dd41",
+  measurementId: "G-G1WFEVRBZJ"
 };
 
 // Initialize Firebase
